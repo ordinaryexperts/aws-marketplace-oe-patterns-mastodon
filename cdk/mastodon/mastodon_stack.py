69d88d16badc211abf0788b61301c89c65a93c24
bubble-teas <yu.iris.d@gmail.com>
2026-07-31 01:07:20 -0500

    Restore dev AMI reference for post-2.6.0 taskcat

--- cdk/mastodon/mastodon_stack.py
+++ cdk/mastodon/mastodon_stack.py
@@ -32,7 +32,7 @@ else:
         template_version = "CICD"
 
 
-AMI_ID="ami-09d705c10912317a1" # ordinary-experts-patterns-mastodon-2.6.0-20260730-0829 (prod AMI, for Marketplace submission)
+AMI_ID="ami-05607bae8bc6e06db" # ordinary-experts-patterns-mastodon-2.6.0-20260730-0619 (dev AMI)
 NEXT_RELEASE_PREFIX="v260"
 
 class MastodonStack(Stack):
